3bb79964485ca8d5edb838426db2909012af3564
lucenasoft <getugbr@gmail.com>
2023-01-14 14:18:09 -0300

    feat: add click accept notify

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,15 @@
 const button = document.querySelector("button")
 
-function contMsn() {
-    tempo = document.querySelectorAll("#display div").length;
-}
+button.addEventListener("click", () => {
+    Notification.requestPermission().then(perm => {
+        if (perm === "granted") {
+            new Notification("Aviso!", {
+                body: "Novas mensagens!",
+                icon: "https://cdn-icons-png.flaticon.com/512/685/685836.png    "
+            })
+        }
+    })
+})
 
 function NotificationPushAll() {
     Notification.requestPermission().then(perm => {
