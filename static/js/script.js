aec6bd5d003208634f1c752fd34e4f01e1ad0e6f
lucenasoft <getugbr@gmail.com>
2023-01-16 23:28:47 -0300

    feat: time-zone add in date

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,30 @@
-const button = document.querySelector("button")
+//const button = document.querySelector("button")
 
-button.addEventListener("click", () => {
+function convertTZ(date, tzString) {
+    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
+}
+
+
+//button.addEventListener("click", () => {
+//    Notification.requestPermission().then(perm => {
+//        if (perm === "granted") {
+//            new Notification("Aviso!", {
+//                body: "Novas mensagens!",
+//                icon: "https://cdn-icons-png.flaticon.com/512/685/685836.png"
+//            })
+//        }
+//    })
+//})
+
+function NotificationPushAll(aviso, mensagem) {
+    pai = document.getElementById("display");
+    ultimoFilho = pai.lastElementChild;
+    aviso = ultimoFilho.firstChild.textContent;
     Notification.requestPermission().then(perm => {
         if (perm === "granted") {
-            new Notification("Aviso!", {
-                body: "Novas mensagens!",
+            new Notification(aviso, {
+                body: "teste",
                 icon: "https://cdn-icons-png.flaticon.com/512/685/685836.png"
-            })
-        }
-    })
-})
-
-function NotificationPushAll() {
-    Notification.requestPermission().then(perm => {
-        if (perm === "granted") {
-            new Notification("Aviso!", {
-                body: "Novas mensagens!",
-                icon: "https://cdn-icons-png.flaticon.com/512/685/685836.png    "
             })
         }
     })
